32979229ec605c1bb9a570297e42e26278451752
krinroman <rkrin@mail.ru>
2020-07-01 03:24:13 +0300

    basic logic app is solved

---
+++ app/src/main/java/com/example/voiceassistent/AI.java
@@ -0,0 +1,89 @@
+package com.example.voiceassistent;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AI {
+    private static final Map<String, String> answers = new HashMap<String, String>(){
+        {
+            put("привет", "Привет");
+            put("как дела", "Неплохо");
+            put("чем занимаешься", "Отвечаю на вопросы");
+        }
+    };
+
+    private static final Map<String, Integer> commands = new HashMap<String, Integer>(){
+        {
+            put("какой сегодня день", 0);
+            put("который час сейчас", 1);
+            put("какой день недели сейчас", 2);
+            put("сколько дней до нового года", 3);
+        }
+    };
+
+    private static String getAnswerToCommand(int idCommand){
+        switch (idCommand){
+            case 0:{
+                Date dateNow = new Date();
+                DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+                return dateFormat.format(dateNow);
+
+            }
+            case 1:{
+                Date dateNow = new Date();
+                DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+                return dateFormat.format(dateNow);
+            }
+            case 2:{
+                Date dateNow = new Date();
+                DateFormat dateFormat = new SimpleDateFormat("E");
+                return dateFormat.format(dateNow);
+            }
+            case 3:{
+                DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+                DateFormat dateYear = new SimpleDateFormat("yyyy");
+                Date dateNow = new Date();
+                try {
+                    Date dateNewYear = dateFormat.parse("01.01."+(Integer.parseInt(dateYear.format(dateNow))+1));
+                    long difference = dateNewYear.getTime() - dateNow.getTime();
+                    int days =  (int)(difference / (24 * 60 * 60 * 1000));
+                    return String.valueOf(days);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                return "0";
+            }
+            default: return null;
+        }
+    }
+
+
+    private static String toNormalForm(String inputString){
+        return inputString.replaceAll("[.,/?!]","").toLowerCase();
+    }
+
+    public static String getAnswer(String question){
+        question = toNormalForm(question);
+
+        int idCommand = -1;
+        for (Map.Entry<String, Integer> command : commands.entrySet()){
+            if(question.contains(command.getKey())){
+                idCommand = command.getValue();
+            }
+        }
+        if(idCommand != -1)
+            return getAnswerToCommand(idCommand);
+        for (Map.Entry<String, String> answer : answers.entrySet()){
+            if(question.contains(answer.getKey())){
+                return answer.getValue();
+            }
+        }
+        return "Вопрос понял. Думаю...";
+    }
+}
--- app/src/main/java/com/example/voiceassistent/MainActivity.java
+++ app/src/main/java/com/example/voiceassistent/MainActivity.java
@@ -1,19 +1,39 @@
 package com.example.voiceassistent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Console;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     protected Button sendButton;
     protected EditText questionText;
     protected TextView chatWindow;
+    protected TextToSpeech textToSpeech;
+    protected boolean ttsEnabled;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("chatText", chatWindow.getText().toString());
+        outState.putString("inputText", questionText.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        chatWindow.setText(savedInstanceState.getString("chatText"));
+        questionText.setText(savedInstanceState.getString("inputText"));
+        super.onRestoreInstanceState(savedInstanceState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +42,44 @@ public class MainActivity extends AppCompatActivity {
         sendButton = findViewById(R.id.sendButton);
         questionText = findViewById(R.id.questionField);
         chatWindow = findViewById(R.id.chatWindow);
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        sendButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                System.out.println("Произошло нажатие на кнопку");
+            public void onClick(View view) {
                 onSend();
-                System.out.println("Вызвался метод onSend()");
             }
         });
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    if (textToSpeech.isLanguageAvailable(new Locale(Locale.getDefault().getLanguage()))
+                            == textToSpeech.LANG_AVAILABLE) {
+                        textToSpeech.setLanguage(new Locale(Locale.getDefault().getLanguage()));
+                    } else {
+                        textToSpeech.setLanguage(Locale.US);
+                    }
+                    textToSpeech.setPitch(1.3f);
+                    textToSpeech.setSpeechRate(0.7f);
+                    ttsEnabled = true;
+                } else  if (status == TextToSpeech.ERROR) {
+                    System.out.println("Произошла ошибка инициализации воспроизведения ответа");
+                    ttsEnabled = false;
+                }
+
+            }
+        });
+
+
     }
+
+
     protected void onSend() {
-        System.out.println("метод onSend()");
         String text = questionText.getText().toString();
-        String answer = "Вопрос понял Думаю…";
-        chatWindow.setText("Вопрос понял, думаю...");//проблема здесь
-        //почему-то chatWindow не появляется текст
-        //сам onClick работает видно в консоли эту строку писала я, надо гуглить тогда как присвоить значение в текст вью др способом
+        String answer = AI.getAnswer(text);
+        chatWindow.append(">> " + text + "\n");
+        chatWindow.append("<< " + answer + "\n");
+        if(ttsEnabled)
+            textToSpeech.speak(answer, TextToSpeech.QUEUE_FLUSH,null, null );
+        questionText.setText("");
     }
 }
